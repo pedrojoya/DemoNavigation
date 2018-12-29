bee63b6385a0f4ab7ab3e5de278b03c98cfa9b3c
pedrojoya <pedrojoya@gmail.com>
2018-12-29 09:33:21 +0100

    v4 - NavigationDrawer + Toolbar + Button + MenuItem + SafeArgs

--- app/src/main/java/es/iessaladillo/pedrojoya/navigationtest/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/navigationtest/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         AppBarConfiguration appBarConfiguration =
             new AppBarConfiguration.Builder(R.id.mainDestination, R.id.galleryDestination, R.id.slideshowDestination)
-            .setDrawerLayout(drawerLayout)
-            .build();
+                .setDrawerLayout(drawerLayout)
+                .build();
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 
