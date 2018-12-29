24666227c33d00682aa28222dd69f6350474a12f
pedrojoya <pedrojoya@gmail.com>
2018-12-29 21:36:37 +0100

    v5 - BottomNavigationView + NavigationDrawer + Toolbar + SafeArgs

--- app/src/main/java/es/iessaladillo/pedrojoya/navigationtest/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/navigationtest/MainActivity.java
@@ -2,6 +2,7 @@ package es.iessaladillo.pedrojoya.navigationtest;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Get navController from activity.
         navController = Navigation.findNavController(this, R.id.navHostFragment);
-        setupToolbar();
-        setupNavigationDrawer();
+        drawerLayout  = findViewById(R.id.drawerLayout);
+        // Title isn't preserved automatically on configuration change with diferent layouts
+        navController.addOnDestinationChangedListener((controller, destination, arguments) ->
+            setTitle(destination.getLabel()));
+        if (drawerLayout != null) {
+            // Navigation drawer mode.
+            setupToolbarWithDrawerLayout();
+            setupNavigationDrawer();
+        } else {
+            // BottomNavigationView mode
+            setupToolbar();
+            setupBottomNavigationView();
+        }
     }
 
-    private void setupToolbar() {
+    private void setupToolbarWithDrawerLayout() {
         Toolbar toolbar = ActivityCompat.requireViewById(this, R.id.toolbar);
-        DrawerLayout drawerLayout = ActivityCompat.requireViewById(this, R.id.drawerLayout);
         setSupportActionBar(toolbar);
         AppBarConfiguration appBarConfiguration =
             new AppBarConfiguration.Builder(R.id.mainDestination, R.id.galleryDestination, R.id.slideshowDestination)
@@ -41,6 +53,21 @@ public class MainActivity extends AppCompatActivity {
     private void setupNavigationDrawer() {
         NavigationView navigationView = ActivityCompat.requireViewById(this, R.id.navigationView);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = ActivityCompat.requireViewById(this, R.id.toolbar);
+        setSupportActionBar(toolbar);
+        AppBarConfiguration appBarConfiguration =
+            new AppBarConfiguration.Builder(R.id.mainDestination, R.id.galleryDestination, R.id.slideshowDestination)
+                .build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+    }
+
+    private void setupBottomNavigationView() {
+        BottomNavigationView bottomNavigationView = ActivityCompat.requireViewById(this, R.id
+            .bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
 }
